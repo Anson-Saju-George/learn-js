7a5ca6b5d2980d1a228f45f05797b2f7b6925f42
Anson Saju George <ansonsaju@karunya.edu.in>
2025-08-25 00:21:38 +0530

    Update

---
+++ DOM.js
@@ -0,0 +1,12 @@
+// Should be run in the browser console in an active web page
+console.dir(document);
+console.log(document.body);
+console.log(document.head);
+console.log(document.title);
+console.log(document.URL);
+console.log(document.links);
+console.log(document.images);
+console.log(document.scripts);
+console.log(document.forms);
+console.log(document.getElementsByTagName('div'));
+console.log(document.querySelectorAll('a'));
