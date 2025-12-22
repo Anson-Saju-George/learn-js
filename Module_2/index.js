66e7b415792b84b67c6d427ced92c2a566182a93
Anson Saju George <ansonsaju@karunya.edu.in>
2025-12-22 09:30:40 +0530

    Woah! Studying aa...

---
+++ Module_2/index.js
@@ -0,0 +1,6 @@
+// Comment: Added a simple console log for demonstration
+// This is my first code in JavaScript module
+console.log("Hello world! \nThis world is beautiful.\nLet's learn JavaScript.");
+console.log("JavaScript is fun and powerful!");
+const message = "Keep learning and practicing.";
+console.log(message);
