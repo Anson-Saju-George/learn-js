caefb8a101855b12a05458f179fad1e46263749a
Anson Saju George <ansonsaju@karunya.edu.in>
2025-12-24 15:32:28 +0530

    Made some dir changes as well

--- Module_2/script.js
+++ Module_2/script.js
@@ -1,29 +1,19 @@
-// This is a simple JavaScript line that logs a message to the console.
-console.log("Hello, World!");
+const product = {
+    title: "Wireless Mouse",
+    rating: 4.5,
+    offers: [
+        { type: "Discount", description: "10% off on first purchase" },
+        { type: "Bundle", description: "Buy 1 Get 1 Free" }
+    ],
+    price: 25.99,
+};
 
-console.error("This is an error message.");
 
-console.warn("This is a warning message.");
-
-console.info("This is an informational message.");
-
-console.debug("This is a debug message.");
-
-console.trace("This is a trace message.");
-
-console.assert(false, "This is an assertion message.");
-console.assert(true, "This is an assertion message.");
-
-console.table([{name: "Alice", age: 30}, {name: "Bob", age: 25}]);
-
-console.time("Timer");
-for (let i = 0; i < 10000000; i++) {
-    // Simulating a time-consuming task
-    let temp = i * i;
-}
-console.timeEnd("Timer");
-
-console.group("Grouped Messages");
-console.log("This is a message inside the group.");
-console.log("Another message inside the group.");
-console.groupEnd();
+console.log(product)
+console.log("Product Title:", product.title);
+console.log("Product Rating:", product.rating);
+console.log("First Offer Type:", product.offers[0].type);
+console.log("First Offer Description:", product.offers[0].description);
+console.log("Second Offer Type:", product.offers[1].type);
+console.log("Second Offer Description:", product.offers[1].description);
+console.log("Product Price:", product.price);
