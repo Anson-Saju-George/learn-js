c1756210d240a9a919c4b82bac1b853a98e373fc
Anson Saju George <ansonsaju@karunya.edu.in>
2025-12-25 21:31:58 +0530

    Learned some Functioning Functions!

--- Module_2/012_functions.js
+++ Module_2/012_functions.js
@@ -14,7 +14,12 @@ const greet = (name) => console.log("Hello, " + name + "!");
 const counterForVowels = (str) => {
     let vowelCount = countVowels(str);
     console.log("Number of vowels in '" + str + "':", vowelCount);
+    // return vowelCount; // Uncommenting this line will return the count or else its undefined
 }
 
 console.log("Module 2 - JavaScript Functions");
 console.log(countVowels("Hello World")); // 3
+x = counterForVowels("Hello World");
+console.log(x); // undefined
+console.log(countVowels("JavaScript Functions")); // 6
+greet("Alice"); // Hello, Alice!
