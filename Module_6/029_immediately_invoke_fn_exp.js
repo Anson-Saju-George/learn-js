63a32ede72cc6f24fc32b99852d3a64b6c1d613a
Anson Saju George <ansonsaju@karunya.edu.in>
2026-01-01 13:03:15 +0530

    IIFE - Immediately Invoked Function Expression!

---
+++ Module_6/029_immediately_invoke_fn_exp.js
@@ -0,0 +1,32 @@
+async function getData(data) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Data received:", data);
+      resolve();
+    }, 2000);
+  });
+}
+
+function api() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("API call completed!");
+      resolve(200);
+    }, 2000);
+  });
+}
+setTimeout(() => {
+  console.log("This message is shown after 10 seconds.");
+}, 10000);
+
+// This is an IIFE (Immediately Invoked Function Expression) which runs as soon as it is defined
+(async function getWeatherData() {
+  console.log("> Fetching weather data...");
+  console.log("> Getting data 1 from API");
+  await getData("> Weather 1");
+  console.log("> Getting data 2 from API");
+  await getData("> Weather 2");
+  console.log("> Getting data 3 from API");
+  await getData("> Weather 3");
+  console.log("All weather data fetched!");
+})();
