f9a1906f30686c66ac4012d965b265c749724c66
Anson Saju George <ansonsaju@karunya.edu.in>
2026-01-01 12:48:25 +0530

    Async programming!

--- Module_6/028_async_await.js
+++ Module_6/028_async_await.js
@@ -1,23 +1,28 @@
 async function getData(data) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log("Data received:", data);
-            resolve();
-        }, 2000);
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Data received:", data);
+      resolve();
+    }, 2000);
+  });
+}
+
+function api() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("API call completed!");
+      resolve(200);
+    }, 2000);
+  });
 }
 
 // Using async-await to handle asynchronous operations sequentially
 async function getWeatherData() {
-    console.log("Fetching weather data...");
-    await getData("Weather 1");
-    await getData("Weather 2");
-    await getData("Weather 3");
-    console.log("All weather data fetched!");
-};
-
+  console.log("Fetching weather data...");
+  await getData("Weather 1");
+  await getData("Weather 2");
+  await getData("Weather 3");
+  console.log("All weather data fetched!");
+}
 
 getWeatherData();
-
-
-
